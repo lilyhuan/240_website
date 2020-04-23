1d1bc952532777b54b4ec7beb7dbe49e736bcfe0
Chris Haas <cwhaas98@gmail.com>
2020-04-23 15:38:29 -0400

    Updated Navbar to have overlay on mobile

--- js/navbar.js
+++ js/navbar.js
@@ -4,6 +4,8 @@ document
 
 function toggleActive() {
   document.getElementById("sidebar").classList.toggle("active");
+  document.getElementById("sidebarCollapse").classList.toggle("active");
+  document.getElementById("overlay").classList.toggle("active");
   document.getElementById("icon").classList.toggle("fa-bars");
   document.getElementById("icon").classList.toggle("fa-window-close");
 }
