f3f0277a497cbc3de9fe175f6ffccde6fd012e4d
Chris Haas <cwhaas98@gmail.com>
2020-04-29 17:55:36 -0400

    Added some content to Projects Page and changed js to work with new changes

--- js/search.js
+++ js/search.js
@@ -1,19 +1,15 @@
 function search() {
-    var input = document.getElementById('search');
-    input = input.value.toLowerCase();
-    var x = document.getElementsByClassName('project');
-    for (i = 0; i < x.length; i++) {
-        var name = x[i].innerHTML.toLowerCase() //project 1
-        console.log(name)
-        // console.log(document.getElementById(name.replace(/ /g,'')))
-        if (!name.includes(input)) {
-            document.getElementById(name.replace(/ /g,'')).style.display = "none";
-        }
-        else {
-            document.getElementById(name.replace(/ /g,'')).style.display = "initial";
-        }
-        console.log(document.getElementById(name.replace(/ /g,'')))
+  var input = document.getElementById("search");
+  input = input.value.toLowerCase();
+  var x = document.getElementsByClassName("project");
+  for (i = 0; i < x.length; i++) {
+    var name = x[i].innerHTML.toLowerCase(); //project 1
+
+    if (!name.includes(input)) {
+      document.getElementById(name.replace(/\s/g, "")).style.display = "none";
+    } else {
+      document.getElementById(name.replace(/\s/g, "")).style.display =
+        "initial";
     }
-
+  }
 }
-
